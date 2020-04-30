414f476e74810c4b6d9eae51b9c67f3d27bbc74c
Okoa Kinsey <okoa.kinsey@stresearch.com>
2020-04-30 13:38:02 -0400

    added view toggle

--- finalProject/src/app/app.component.ts
+++ finalProject/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import populationDensity from './../assets/country-by-population-density.json';
 import population from './../assets/country-by-population.json';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,27 @@ export class AppComponent {
   title = 'finalProject';
   public populationDensity:{country:string, density:string}[] = populationDensity;
   public population:{country:string, population:string}[] = population;
+
+  toggled = false;
+
+  toggleView(args){ 
+    if(!this.toggled){
+      // $("#globe, #country_list, #graph").addClass("toggled");
+      args.forEach((i) => {
+        var elem = document.querySelector(`#${i}`); 
+        elem.classList.add("toggled")});
+      this.toggled = true;
+    }
+    else{ 
+      args.forEach((i) => {
+        var elem = document.querySelector(`#${i}`); 
+        elem.classList.remove("toggled")});
+      this.toggled = false;
+  }
+}
+
+  ngOnInit(){
+
+  }
+
 }
